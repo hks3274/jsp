d909cffbe39bb33520af0b31a8c19cecaa7b287c
sooYeon <hks3274@naver.com>
2024-04-25 09:58:00 +0900

    20240425_first

---
+++ src/main/java/study/j0424/Test2.java
@@ -0,0 +1,27 @@
+package study.j0424;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//@WebServlet("/Test2") //url맵핑의 주소이다...
+//@WebServlet("/T2") 
+@WebServlet("/T22222") 
+public class Test2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("이곳은 GET메소드 입니다.");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	
+}
